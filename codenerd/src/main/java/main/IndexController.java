5fca7848e424544f5ed1d1f1a49951a779cf2dc1
Rudi.Kershaw <alexander_kershaw@hotmail.co.uk>
2015-06-20 17:15:35 +0100

    Rudi Kershaw - Implement index controller and thymeleaf index.html

--- codenerd/src/main/java/main/IndexController.java
+++ codenerd/src/main/java/main/IndexController.java
@@ -1,16 +1,17 @@
 package main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Rudi Kershaw on 20/06/2015.
  */
-@RestController
+@Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String home(){
-        return "So far so good!";
+    public String index(Model model){
+        return "index";
     }
 }
