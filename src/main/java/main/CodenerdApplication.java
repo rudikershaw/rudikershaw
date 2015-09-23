564ddec0306cd66dce93149715fa2a2653d6c1a6
Rudi.Kershaw <alexander_kershaw@hotmail.co.uk>
2015-09-23 18:19:51 +0100

    Rudi Kershaw - Add 1 week image caching. Add hover to search buttons.

--- src/main/java/main/CodenerdApplication.java
+++ src/main/java/main/CodenerdApplication.java
@@ -2,13 +2,24 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /** The Code Nerd application configuration. */
 @SpringBootApplication
-public class CodenerdApplication {
+public class CodenerdApplication extends WebMvcConfigurerAdapter {
+
+    private static final int CACHE_TIME = 604800;
 
     /** Start here. */
     public static void main(String[] args) {
         SpringApplication.run(CodenerdApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/images/")
+                .setCachePeriod(CACHE_TIME);
     }
 }
