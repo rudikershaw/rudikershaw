d4a581b62dad3be87cddaa3761672f9b3a5231f3
Rudi.Kershaw <alexander_kershaw@hotmail.co.uk>
2016-10-05 18:09:50 +0100

    Rudi Kershaw - Fix to binary search animation script.

--- src/main/resources/static/scripts/search-graphics.js
+++ src/main/resources/static/scripts/search-graphics.js
@@ -2,17 +2,17 @@ $(document).ready(function(){
     $('.container input[name="e-search-go"]').click(function(){
         var button = $(this);
         var searchFor = $('.container input[name="e-search-for"]').val();
-        button.parent().find('li').removename('checked found');
+        button.parent().find('li').removeClass('checked found');
         setTimeout(function(){
             button.parent().find('li').each(function(index, element){
                 if($(element).text() == searchFor){
                     setTimeout(function(){
-                        $(element).addname('found');
+                        $(element).addClass('found');
                     }, 500*index);
                     throw "Found";
                 } else {
                     setTimeout(function(){
-                        $(element).addname('checked');
+                        $(element).addClass('checked');
                     }, 500*index);
                 }
 
@@ -23,7 +23,7 @@ $(document).ready(function(){
     $('.container input[name="b-search-go"]').click(function(){
         var button = $(this);
         var find = $('.container input[name="b-search-for"]').val();
-        button.parent().find('li').removename('checked found');
+        button.parent().find('li').removeClass('checked found');
         setTimeout(function(){
             var bottom = 0;
             var top = 13;
@@ -34,19 +34,19 @@ $(document).ready(function(){
                 var middle = (bottom + top)/2 | 0;
                 if($(lis.get(middle)).text() == find) {
                     setTimeout(function(){
-                        $(lis.get(middle)).addname('found');
+                        $(lis.get(middle)).addClass('found');
                     },500*time);
                     throw "Found";
                 } else if(parseInt($(lis.get(middle)).text()) > find) {
                     (function(middle){
                     setTimeout(function(){
-                        $(lis.get(middle)).addname('checked');
+                        $(lis.get(middle)).addClass('checked');
                     },500*time)}(middle));
                     top = middle - 1;
                 } else if(parseInt($(lis.get(middle)).text()) < find) {
                     (function(middle){
                     setTimeout(function(){
-                        $(lis.get(middle)).addname('checked');
+                        $(lis.get(middle)).addClass('checked');
                     },500*time)}(middle));
                     bottom = middle + 1;
                 }
