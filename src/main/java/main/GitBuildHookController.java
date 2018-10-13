4fcdb5e57efd3a7eae1420269710377ac62f633b
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2018-10-13 14:43:33 +0100

    Rudi Kershaw - Fix silly mistake made when rushing.

--- src/main/java/main/GitBuildHookController.java
+++ src/main/java/main/GitBuildHookController.java
@@ -1,12 +1,15 @@
 package main;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /** Redirects to the Github project so that group-id of the plugin has an existing end-point. */
+@Controller
 public class GitBuildHookController {
 
     /** Request mapping for redirecting from Group ID to the GitHub project. */
-    @RequestMapping("/gitbuildhook")
+    @RequestMapping(value = "/gitbuildhook", method = {RequestMethod.GET, RequestMethod.POST})
     public String redirectToProject()
     {
         return "redirect:https://github.com/rudikershaw/git-build-hook";
