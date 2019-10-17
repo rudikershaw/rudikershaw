8a7edc787b78f44c6ddc15eff48e00766d21d1ac
Rudi Kershaw <rudi.kershaw@cognitran.com>
2019-10-17 09:31:01 +0100

    Add Moesif analytics configuration.

--- src/main/java/main/CodenerdApplication.java
+++ src/main/java/main/CodenerdApplication.java
@@ -1,12 +1,19 @@
 package main;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.moesif.servlet.MoesifFilter;
+
 /** The Code Nerd application configuration. */
+@Configuration
 @EnableScheduling
 @SpringBootApplication
 public class CodenerdApplication implements WebMvcConfigurer {
@@ -27,5 +34,15 @@ public class CodenerdApplication implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/static/images/")
                 .setCachePeriod(CACHE_TIME);
+    }
+
+    /**
+     * Configure the Moesif analytics filter.
+     * @return the Filter.
+     */
+    @Bean
+    public Filter moesifFilter() {
+        return new MoesifFilter("eyJhcHAiOiI2MTc6MjUxIiwidmVyIjoiMi4wIiwib3JnIjoiMjA3OjI2NCIsI"
+                                + "mlhdCI6MTU3MTI3MDQwMH0.k7mv34JLYi4NPBPzP4RMJnzSyFmCc2z1uJrxm-VrMgg");
     }
 }
