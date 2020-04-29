718ce864afd049eee268ed1370073e6b16ce04de
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2020-04-29 21:13:05 +0100

    #13 Add latest read to activity on home page.

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
     $.ajax({ url: '/latest-tweet' }).done(function(data){
         $('#latest-tweet').replaceWith(data);
     });
+
+    $.ajax({ url: '/data/bibliography.json' }).done(function(data){
+        var latestRead = data.bibliography[0];
+        $('#latest-read .title').text(latestRead.title);
+        $('#latest-read .author').text('by ' + latestRead.author);
+        $('#latest-read .synopsis').text(latestRead.synopsis);
+        $('#latest-read .review').text(latestRead.review);
+        $('#latest-read').show();
+    });
 });
