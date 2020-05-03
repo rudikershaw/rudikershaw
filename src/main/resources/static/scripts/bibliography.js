5573408ddff9258d4eba258a80ed0e860877fcec
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2020-05-03 15:53:56 +0100

    #13 Add time-line-esque styling to the bibliography.

--- src/main/resources/static/scripts/bibliography.js
+++ src/main/resources/static/scripts/bibliography.js
@@ -3,15 +3,43 @@
     var list = document.querySelector('#bibliography-list');
     var request = new XMLHttpRequest();
 
+    function daysBetweenDates(date1, date2) {
+        const oneDayInMiliseconds = 24 * 60 * 60 * 1000;
+        return Math.round(Math.abs((date1 - date2) / oneDayInMiliseconds));
+    }
+
     function populatePageWithBibliography(response) {
+        var newEntry;
+        var newDateDividor;
+        var currentFinishedDate;
+        var previousFinishedDate = new Date();
+
         if (response && response.bibliography) {
             response.bibliography.forEach(function (item){
-                var newEntry = template.cloneNode(true);
+                newEntry = template.cloneNode(true);
+                currentFinishedDate = new Date(item.dateFinished);
                 newEntry.querySelector('cite').textContent = item.title;
                 newEntry.querySelector('.author').textContent = item.author;
+                newEntry.querySelector('.synopsis').textContent = item.synopsis;
                 newEntry.removeAttribute('style');
+                newEntry.removeAttribute('id');
+
+                if (previousFinishedDate.getFullYear() !== currentFinishedDate.getFullYear()) {
+                    newDateDividor = list.querySelector('.date-divider').cloneNode(true);
+                    newDateDividor.textContent = currentFinishedDate.getFullYear();
+                    list.appendChild(newDateDividor);
+                }
+                if (previousFinishedDate) {
+                    newEntry.style.marginTop = daysBetweenDates(previousFinishedDate, currentFinishedDate) + 'px';
+                }
+
                 list.appendChild(newEntry);
+                previousFinishedDate  = currentFinishedDate;
             });
+
+            newDateDividor = list.querySelector('.date-divider').cloneNode(true);
+            newDateDividor.textContent = 'Etc';
+            list.appendChild(newDateDividor);
         }
     }
 
