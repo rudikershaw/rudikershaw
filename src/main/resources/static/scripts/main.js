0861ed210d9926180eb9c15959961541314a4e0e
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2020-05-10 22:08:53 +0100

    Reformat main.js and switch to using outerHTML instead of DOMParser.

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -1,21 +1,23 @@
 import 'vanilla-fade/dist/esm/fadeOut';
 
 (() => {
+    fetch('/latest-tweet')
+        .then((data) => data.text())
+        .then((text) => {
+            document.querySelector('#latest-tweet').outerHTML = text;
+        });
+
+    fetch('/data/bibliography.json')
+        .then((data) => data.json())
+        .then((json) => {
+            const latestRead = json.bibliography[0];
+            document.querySelector('#latest-read .title').textContent = latestRead.title;
+            document.querySelector('#latest-read .author').textContent = 'by ' + latestRead.author;
+            document.querySelector('#latest-read .synopsis').textContent = latestRead.synopsis;
+            document.querySelector('#latest-read .review').textContent = latestRead.review;
+            document.querySelector('#latest-read').style.display = null;
+        });
+
     const curtain = document.querySelector('div.faded-background-cover');
     curtain.fadeOut(1600, 'linear', () => curtain.style.display = 'none');
-
-    fetch('/latest-tweet').then((data) => data.text()).then((text) => {
-        const placeholder = document.querySelector('#latest-tweet');
-        const body = new DOMParser().parseFromString(text, 'text/html').body.childNodes[0];
-        placeholder.parentNode.replaceChild(body, placeholder);
-    });
-
-    fetch('/data/bibliography.json').then((data) => data.json()).then((json) => {
-        const latestRead = json.bibliography[0];
-        document.querySelector('#latest-read .title').textContent = latestRead.title;
-        document.querySelector('#latest-read .author').textContent = 'by ' + latestRead.author;
-        document.querySelector('#latest-read .synopsis').textContent = latestRead.synopsis;
-        document.querySelector('#latest-read .review').textContent = latestRead.review;
-        document.querySelector('#latest-read').style.display = null;
-    });
 })();
