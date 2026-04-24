6b74c4c06bbaf33048cc058f56bcab7f2a55dad3
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2026-04-24 23:00:19 +0100

    Add sitemap generation and robot.txt

---
+++ src/main/java/main/SitemapController.java
@@ -0,0 +1,55 @@
+package main;
+
+import static main.articles.DefaultArticleController.ARTICLES;
+
+import java.net.MalformedURLException;
+
+import com.redfin.sitemapgenerator.ChangeFreq;
+import com.redfin.sitemapgenerator.WebSitemapGenerator;
+import com.redfin.sitemapgenerator.WebSitemapUrl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import main.dynamics.entities.Article;
+
+/** Controller that serves a sitemap.xml for search-engine discovery. */
+@Controller
+public class SitemapController {
+
+    /** Base URL from the properties file. */
+    @Value("${base.url}")
+    private String baseUrl;
+
+    /**
+     * Generate and return the site's sitemap.xml.
+     *
+     * @return the XML body with the correct content type.
+     * @throws MalformedURLException if the configured base URL is invalid.
+     */
+    @Cacheable("sitemap")
+    @GetMapping(value = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<String> sitemap() throws MalformedURLException {
+        final WebSitemapGenerator generator = new WebSitemapGenerator(baseUrl);
+
+        generator.addUrl(new WebSitemapUrl.Options(baseUrl)
+                .changeFreq(ChangeFreq.WEEKLY).priority(1.0).build());
+        generator.addUrl(new WebSitemapUrl.Options(baseUrl + "bibliography")
+                .changeFreq(ChangeFreq.MONTHLY).priority(0.8).build());
+
+        for (final Article article : ARTICLES) {
+            generator.addUrl(new WebSitemapUrl.Options(baseUrl + article.getPath())
+                    .lastMod(article.getPublished())
+                    .changeFreq(ChangeFreq.YEARLY)
+                    .priority(0.8)
+                    .build());
+        }
+
+        final String xml = generator.writeAsStrings().stream().findFirst().orElseThrow();
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_XML).body(xml);
+    }
+}
